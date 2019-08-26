36c806d97501aa981c96e9c30c74411936b47ae5
Mike Pieper <mpieper636@gmail.com>
2019-08-26 16:55:38 -0500

    utils file wasn't saved

--- Localization/particle_filter/utils.py
+++ Localization/particle_filter/utils.py
@@ -8,31 +8,4 @@ def gauss_likelihood(x, sigma):
     return p
 
 
-def calculate_covariance(mu, px, pw):
-    cov = np.zeros((3, 3))
 
-    for i in range(px.shape[1]):
-        dx = (px[:, i] - mu)[0:3]
-        cov += pw[0, i] * dx.dot(dx.T)
-
-    return cov
-
-
-def low_variance_resampling(px, pw):
-    Neff = 1.0 / (pw.dot(pw.T))[0, 0]  # Effective particle number
-    if Neff < NTh:
-        wcum = np.cumsum(pw)
-        base = np.cumsum(pw * 0.0 + 1 / NP) - 1 / NP
-        resampleid = base + np.random.rand(base.shape[0]) / NP
-
-        inds = []
-        ind = 0
-        for ip in range(NP):
-            while resampleid[ip] > wcum[ind]:
-                ind += 1
-            inds.append(ind)
-
-        px = px[:, inds]
-        pw = np.zeros((1, NP)) + 1.0 / NP  # init weight
-
-    return px, pw
